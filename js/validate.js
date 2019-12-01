d5817a08681e723f0f3de45e3a4a8487e245bc37
elisabetros <elberoest@gmail.com>
2019-12-01 20:40:10 +0100

    validation

--- js/validate.js
+++ js/validate.js
@@ -1,121 +1,39 @@
 // let form = document.querySelector('form');
-let allInputs = document.querySelectorAll('input');
-let textarea = document.querySelectorAll('textarea');
+let allInputs = document.querySelectorAll('[data-type=string]');
+// console.log(allInputs)
 let emailInput = document.querySelector('input[data-type=email]');
-const submitBtn= document.querySelector("input[type=submit]")
+const submitBtn = document.querySelector('input[type=submit]');
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+emailInput.addEventListener("input", function(){
+  let form = emailInput.parentElement.parentElement;
+  if(re.test(String(emailInput.value).toLowerCase())){
+    emailInput.classList.remove('error')
+    emailInput.classList.add('valid')
+  }else{
+    emailInput.classList.add('error')
+    emailInput.classList.remove('valid')
+    } 
+})
 
-// textarea.addEventListener('input', function(){
-//     console.log("input");
-// } )
-// allInputs.forEach(input => {
-//     input.addEventListener('input', function(){
-//         let form = input.parentElement.parentElement;
-      
-//         console.log(form.checkValidity());
-//          if(re.test(String(emailInput.value).toLowerCase())){
-//              emailInput.classList.remove('inputError')
-//             form.querySelector('.button').disabled=false;
-//         }else{
-//             emailInput.classList.add('inputError')
-//             form.querySelector('.button').disabled = true;
-//         } 
-//         if()
-//         {
-//             form.querySelector('.button').disabled=false;
-//             input.classList.add("error")
-//         }else{
-//             form.querySelector('.button').disabled = true;
-//             input.classList.remove("error")
-//         }
-  
-//     })
-// });
-submitBtn.addEventListener("click", function(){
-    event.preventDefault();
-    fnbIsFormValid(document.querySelector("#frmContact"))
-} );
-
-
-function fnbIsFormValid(oForm){
-    fvDo( oForm.querySelectorAll('input[data-type]'), function(oElement){
-      oElement.classList.remove('error')
-    })
-    fvDo( oForm.querySelectorAll('input[data-type]'), function(oElement){
-      var sValue = oElement.value
-      var sDataType = oElement.getAttribute('data-type') // $(oInput).attr('data-type')
-      var iMin = oElement.getAttribute('data-min') //$(oInput).attr('data-min')
-      var iMax = oElement.getAttribute('data-max') // $(oInput).attr('data-max')  
-      switch(sDataType){
-        case 'string':
-          if( sValue.length < iMin || sValue.length > iMax ){ 
-            oElement.classList.add('error')
-          }
-        break
-        case 'integer':
-          if( !parseInt(sValue) || parseInt(sValue) < parseInt(iMin) || parseInt(sValue) > parseInt(iMax) ){ 
-            oElement.classList.add('error')
-          }
-        break
-        case 'email':
-          var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          if( re.test(String(sValue).toLowerCase()) == false ){ 
-            oElement.classList.add('error')
-          }     
-        break      
-        default:        
-      }
-    })  
-  
-    if( oForm.querySelectorAll('input.error').length ){ return false }
-    return true;
-  }
-
-
-
-function fnbIsFormValid(oForm){
-    fvDo( oForm.querySelectorAll('input[data-type]'), function(oElement){
-      oElement.classList.remove('error')
-    })
-    
-    fvDo( oForm.querySelectorAll('[data-type]'), function(oElement){
-      var sValue = oElement.value
-      var sDataType = oElement.getAttribute('data-type') // $(oInput).attr('data-type')
-      var iMin = oElement.getAttribute('data-min') //$(oInput).attr('data-min')
-      var iMax = oElement.getAttribute('data-max') // $(oInput).attr('data-max')  
-      switch(sDataType){
-        case 'string':
-          if( sValue.length < iMin || sValue.length > iMax ){ 
-            oElement.classList.add('error')
-          }
-        break
-        case 'integer':
-          if( !parseInt(sValue) || parseInt(sValue) < parseInt(iMin) || parseInt(sValue) > parseInt(iMax) ){ 
-            oElement.classList.add('error')
-          }
-        break
-        case 'email':
-          var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          if( re.test(String(sValue).toLowerCase()) == false ){ 
-            oElement.classList.add('error')
-          }     
-        break      
-        default:        
-      }
-    })  
-  
-    if( oForm.querySelectorAll('input.error').length ){ return false }
-    return true;
-  }
-
-
-
-
-
-  function fvDo( aElements, fvCallback ){
-      console.log(aElements)
-    for(var i = 0; i < aElements.length; i++){
-      fvCallback( aElements[i] )
+allInputs.forEach(input => {
+  input.addEventListener('input', function(){
+    let sValue = input.value
+    let iMin = input.getAttribute('data-min') 
+    let iMax = input.getAttribute('data-max')
+    if( sValue.length < iMin || sValue.length > iMax ){ 
+      input.classList.add('error')
+      input.classList.remove('valid')
+    }else{
+      input.classList.remove("error");
+      input.classList.add("valid")
     }
-  }
+    if( document.querySelectorAll('.error').length===0 && document.querySelectorAll(".valid").length===3 ){
+      console.log("valid")
+      console.log(document.querySelectorAll("input.valid").length)
+       submitBtn.disabled=false;
+      }else{
+        submitBtn.disabled=true;
+    }
+  })
+});
